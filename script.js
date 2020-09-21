d962b72fb1c1f96d65bd78c8b621808903eabf09
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-09-21 17:42:33 -0400

    Finished while loop, if/else statements and cancel password generator

--- script.js
+++ script.js
@@ -4,15 +4,35 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var passwordLength = 0;
-  while (passwordLength < 8 || passwordLength > 128) {
-    prompt("Choose between 8 and 128 characters.");
+  while (
+    passwordLength < 8 ||
+    passwordLength > 128 ||
+    isNaN(passwordLength) ||
+    Number.isInteger(+passwordLength) != true
+  ) {
+    passwordLength = prompt("Choose between 8 and 128 characters.");
+    if (passwordLength === null) {
+      // allow to go to the next question
+      break;
+    } else {
+      if (passwordLength < 8) {
+        alert("Password length must be longer than 8 characters");
+      } else if (passwordLength > 128) {
+        alert("Password length must be less than 129 characters.");
+      } else if (
+        isNaN(passwordLength) ||
+        Number.isInteger(+passwordLength) != true
+      ) {
+        alert(
+          "Password length must be a whole number greater than 8 but less than 129"
+        );
+      }
+    }
   }
-  if (passwordLength < 8) {
-    alert(
-      "Password length must be longer than 8 characters");
-  } else if (passwordLength > 128) {
-    "Password length must be less than 129 characters."
+  if (passwordLength === null) {
+    return "Cancelled Generated Password";
   }
+  // console.log(+passwordLength);
   var lowerCase = confirm("Click OK to use lowercase letters.");
   var upperCase = confirm("Click OK to use uppercase letters.");
   var numeric = confirm("Click OK to use numbers.");
