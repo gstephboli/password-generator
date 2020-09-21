6352d8bbf2be999af457a3e5312faeadf14b6276
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-09-21 17:15:18 -0400

    [WIP] Setting up password length parameters

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// Set variables for each item than can be included in the passwork
-var lowerCase;
-var upperCase;
-var numeric;
-var specChar;
-var passwordLength = >= 8 || <= 128;
 
 // Write password to the #password input
 function writePassword() {
-  prompt("Choose between 8 and 128 characters.");
-  lowerCase = confirm("Click OK to use lowercase letters.");
-  upperCase = confirm("Click OK to use uppercase letters.");
-  numeric = confirm("Click OK to use numbers.");
-  specChar = confirm("Click OK to use special characters.");
+  var passwordLength = 0;
+  while (passwordLength < 8 || passwordLength > 128) {
+    prompt("Choose between 8 and 128 characters.");
+  }
+  if (passwordLength < 8) {
+    alert(
+      "Password length must be longer than 8 characters");
+  } else if (passwordLength > 128) {
+    "Password length must be less than 129 characters."
+  }
+  var lowerCase = confirm("Click OK to use lowercase letters.");
+  var upperCase = confirm("Click OK to use uppercase letters.");
+  var numeric = confirm("Click OK to use numbers.");
+  var specialChar = confirm("Click OK to use special characters.");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-
 function generatePassword() {
-    var length = 8,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-+={}’",
-        retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal;
+  var length = 8,
+    charset =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-+={}’",
+    retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
