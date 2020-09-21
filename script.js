8397ed3c67a2bd098cffc29376499580d3a135a9
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-09-21 14:50:36 -0400

    Added special characters to function generatePassword .js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function writePassword() {
 
 function generatePassword() {
     var length = 8,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-+={}’",
         retVal = "";
     for (var i = 0, n = charset.length; i < length; ++i) {
         retVal += charset.charAt(Math.floor(Math.random() * n));
